9d0182b7fd2dbca70642bcc621ad629b117d9cd1
EunsuSeo01 <eunsu0613@naver.com>
2023-08-04 19:49:34 +0900

    [Chore] #5 Xcode 프로젝트 생성 후 No storyboard 세팅

---
+++ Milestone/Milestone/Controller/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Milestone
+//
+//  Created by 서은수 on 2023/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
---
+++ Milestone/Milestone/Support/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Milestone
+//
+//  Created by 서은수 on 2023/08/04.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+}
+
