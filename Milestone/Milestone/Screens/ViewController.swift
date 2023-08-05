0aac1f5dbc5bc98a7a09eb6ff7cb87baff0aff2a
EunsuSeo01 <eunsu0613@naver.com>
2023-08-05 23:55:03 +0900

    [Chore] #5 Extension 추가
    UIFont+, UIColor+, UIView+, UILabel+ 추가

--- Milestone/Milestone/Screens/ViewController.swift
+++ Milestone/Milestone/Screens/ViewController.swift
@@ -15,9 +15,19 @@ import Alamofire
 import Moya
 
 class ViewController: UIViewController {
+    
+    let testLabel = UILabel()
+        .then {
+            $0.text = "MileStone"
+            $0.font = .pretendard(.bold, ofSize: 30)
+        }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        view.addSubView(testLabel)
+        testLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
 }
