7bf1d6a61a0db7bff000c58e658fbf692bbd67b5
EunsuSeo01 <eunsu0613@naver.com>
2023-08-06 04:18:27 +0900

    [Chore] #5 폴더 구조 변경 및 ImageLiteral과 Logger 추가
    이미지 적용할 때 사용할 UIImage+와 ImageLiteral 추가
    로그 찍을 때 사용할 Logger 추가
    Global 폴더 추가

--- Milestone/Milestone/Screens/ViewController.swift
+++ Milestone/Milestone/Screens/ViewController.swift
@@ -7,12 +7,8 @@
 
 import UIKit
 
-import RxSwift
-import RxCocoa
 import SnapKit
 import Then
-import Alamofire
-import Moya
 
 class ViewController: UIViewController {
     
@@ -21,13 +17,23 @@ class ViewController: UIViewController {
             $0.text = "MileStone"
             $0.font = .pretendard(.bold, ofSize: 30)
         }
+    let testImage = UIImageView()
+        .then {
+            $0.image = ImageLiteral.imgStone.resize(to: CGSize(width: 60, height: 60))
+        }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.addSubView(testLabel)
+
+        view.addSubViews([testLabel, testImage])
         testLabel.snp.makeConstraints { make in
             make.center.equalToSuperview()
         }
+        testImage.snp.makeConstraints { make in
+            make.bottom.equalTo(testLabel.snp.top).offset(-10)
+            make.centerX.equalToSuperview()
+        }
+        Logger.debugDescription(testImage)
     }
 }
