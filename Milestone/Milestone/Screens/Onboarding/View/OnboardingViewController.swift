abae2a109e58f6ffa33371d16b3c130afe66f6f8
Parkjju <rudwns3927@gmail.com>
2023-08-13 12:48:58 +0900

    [Test] #16 온보딩 뷰 컨트롤러 임시 생성
    코디네이터 속성 임시추가

---
+++ Milestone/Milestone/Screens/Onboarding/View/OnboardingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingViewController.swift
+//  Milestone
+//
+//  Created by 박경준 on 2023/08/13.
+//
+
+import UIKit
+
+class OnboardingViewController: BaseViewController {
+    
+    var coordinator: OnboardingFlow?
+    
+}
