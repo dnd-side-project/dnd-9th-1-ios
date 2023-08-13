8f0cd1fa41fa8a1dfece0963a29d04597d480753
Parkjju <rudwns3927@gmail.com>
2023-08-13 12:45:07 +0900

    [Feat] #16 앱 델리게이트에 루트 AppCoordinator 추가
    앱 코디네이터 window속성에 연결

--- Milestone/Milestone/Global/Support/AppDelegate.swift
+++ Milestone/Milestone/Global/Support/AppDelegate.swift
@@ -10,13 +10,14 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    var coordinator: AppCoordinator?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        window?.rootViewController = MainViewController()
+        coordinator = AppCoordinator(window: window!)
+        coordinator?.start()
         
         return true
     }
