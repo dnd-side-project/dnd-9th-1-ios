f94975b6105d0694f2e51ea4af8b067e1e0189c7
Parkjju <rudwns3927@gmail.com>
2023-08-16 00:03:59 +0900

    [Feat] #39 Goal 임시모델에 회고 작성여부 속성 추가
    완료여부 검증 후 버튼 다르게 표시하도록 로직 추가

--- Milestone/Milestone/Screens/CompletionBox/ViewModel/CompletionViewModel.swift
+++ Milestone/Milestone/Screens/CompletionBox/ViewModel/CompletionViewModel.swift
@@ -14,6 +14,7 @@ struct Goal: IdentifiableType, Equatable {
     let title: String
     let startDate: Date
     let endDate: Date
+    let isCompleted: Bool
     let identity: Int
 }
 
@@ -21,7 +22,7 @@ typealias CompletionSectionModel = AnimatableSectionModel<Int, Goal>
 
 class CompletionViewModel {
     
-    private var goalList = [Goal(title: "", startDate: Date(), endDate: Date(), identity: -1), Goal(title: "포토샵 자격증 따기", startDate: Date(), endDate: Date().addingTimeInterval(100), identity: 0), Goal(title: "자격증 포토샵 따기", startDate: Date().addingTimeInterval(100), endDate: Date().addingTimeInterval(200), identity: 1), Goal(title: "스위프트 마스터하기", startDate: Date().addingTimeInterval(300), endDate: Date().addingTimeInterval(500), identity: 2)]
+    private var goalList = [Goal(title: "", startDate: Date(), endDate: Date(),isCompleted: true, identity: -1), Goal(title: "포토샵 자격증 따기", startDate: Date(), endDate: Date().addingTimeInterval(100), isCompleted: true, identity: 0), Goal(title: "자격증 포토샵 따기", startDate: Date().addingTimeInterval(100), endDate: Date().addingTimeInterval(200), isCompleted: true, identity: 1), Goal(title: "스위프트 마스터하기", startDate: Date().addingTimeInterval(300), endDate: Date().addingTimeInterval(500), isCompleted: false, identity: 2)]
     
 //    private var goalList: [Goal] = []
     
@@ -35,8 +36,9 @@ class CompletionViewModel {
         let ds = RxTableViewSectionedAnimatedDataSource<CompletionSectionModel> { dataSource, tableView, indexPath, goal in
             guard let cell = tableView.dequeueReusableCell(withIdentifier: CompletionTableViewCell.identifier, for: indexPath) as? CompletionTableViewCell else { return UITableViewCell() }
             
-            if(indexPath.section == 0) {
+            if indexPath.section == 0 {
                 let completionView = CompletionAlertView()
+                
                 cell.contentView.addSubview(completionView)
                 completionView.snp.makeConstraints { make in
                     make.margins.equalTo(cell.contentView.snp.margins)
@@ -57,6 +59,20 @@ class CompletionViewModel {
             let endDateString = dateFormatter.string(from: goal.endDate)
             
             cell.dateLabel.text = startDateString + " - " + endDateString
+            
+            if goal.isCompleted {
+                cell.button.setTitle("회고 보기", for: .normal)
+                cell.button.setTitleColor(.primary, for: .normal)
+                cell.button.backgroundColor = .white
+                cell.button.layer.borderColor = UIColor.secondary01.cgColor
+                cell.button.layer.borderWidth = 1
+            } else {
+                cell.button.setTitle("회고 작성하기", for: .normal)
+                cell.button.setTitleColor(.primary, for: .normal)
+                cell.backgroundColor = .secondary03
+                cell.layer.borderColor = UIColor.clear.cgColor
+            }
+            
             return cell
         }
         
