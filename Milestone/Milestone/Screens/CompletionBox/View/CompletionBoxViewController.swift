0c97dd908cd4c8f7ea3d323d0ab0dfa7911672fc
Parkjju <rudwns3927@gmail.com>
2023-08-15 23:51:11 +0900

    [Feat] #39 함수위치 변경 및 말풍선 뷰 삽입

--- Milestone/Milestone/Screens/CompletionBox/View/CompletionBoxViewController.swift
+++ Milestone/Milestone/Screens/CompletionBox/View/CompletionBoxViewController.swift
@@ -43,6 +43,21 @@ class CompletionBoxViewController: BaseViewController, ViewModelBindableType {
             $0.register(cell: CompletionTableViewCell.self, forCellReuseIdentifier: CompletionTableViewCell.identifier)
         }
     
+    private let bubbleView = UIView()
+        .then {
+            $0.layer.cornerRadius = 20
+            $0.backgroundColor = .gray05
+        }
+    
+    private let triangle = TriangleView()
+    
+    private let bubbleLabel = UILabel()
+        .then {
+            $0.font = UIFont.pretendard(.semibold, ofSize: 14)
+            $0.textColor = .white
+            $0.text = "이룬 목표에 대한 회고를 자세히 기록해보세요!"
+        }
+    
     // MARK: Properties
     
     var viewModel: CompletionViewModel!
@@ -57,10 +72,32 @@ class CompletionBoxViewController: BaseViewController, ViewModelBindableType {
             .disposed(by: disposeBag)
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        guard let cell = tableView.visibleCells.first else { return }
+        let alertView = cell.contentView.subviews.last as! CompletionAlertView
+        
+        viewModel.completionList
+            .map { $0.isEmpty }
+            .bind(to: cell.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        viewModel.completionList
+            .map {
+                let string = NSMutableAttributedString(string: "총 \($0.count - 1)개의 목표 회고를 작성할 수 있어요!")
+                string.setColorForText(textForAttribute: "총 \($0.count - 1)개의 목표 회고", withColor: .pointPurple)
+                return string
+            }
+            .bind(to: alertView.label.rx.attributedText)
+            .disposed(by: disposeBag)
+        
+        setAdditionalLayout()
+    }
+    
     // MARK: functions
     
     override func render() {
-        view.addSubViews([emptyImageView, label, tableView])
+        view.addSubViews([emptyImageView, label, tableView, bubbleView, triangle])
         
         emptyImageView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(100)
@@ -86,26 +123,6 @@ class CompletionBoxViewController: BaseViewController, ViewModelBindableType {
         bindViewModel()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        guard let cell = tableView.visibleCells.first else { return }
-        let alertView = cell.contentView.subviews.last as! CompletionAlertView
-        
-        viewModel.completionList
-            .map { $0.isEmpty }
-            .bind(to: cell.rx.isHidden)
-            .disposed(by: disposeBag)
-        
-        viewModel.completionList
-            .map {
-                let string = NSMutableAttributedString(string: "총 \($0.count - 1)개의 목표 회고를 작성할 수 있어요!")
-                string.setColorForText(textForAttribute: "총 \($0.count - 1)개의 목표 회고", withColor: .pointPurple)
-                return string
-            }
-            .bind(to: alertView.label.rx.attributedText)
-            .disposed(by: disposeBag)
-    }
-    
     func bindViewModel() {
         viewModel.completionList
             .bind(to: tableView.rx.items(dataSource: viewModel.dataSource))
@@ -114,7 +131,44 @@ class CompletionBoxViewController: BaseViewController, ViewModelBindableType {
         viewModel.completionList
             .map { !$0.isEmpty }
             .bind(to: emptyImageView.rx.isHidden, label.rx.isHidden)
-            .disposed(by: disposeBag) 
+            .disposed(by: disposeBag)
+        
+        tableView.rx.didScroll
+            .subscribe { [weak self] _ in
+                self?.bubbleView.isHidden = true
+                self?.bubbleLabel.isHidden = true
+                self?.triangle.isHidden = true
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    func setAdditionalLayout() {
+        
+        triangle.frame = CGRect(x: view.frame.width / 2, y: view.frame.height / 2, width: 18, height: 18)
+        triangle.backgroundColor = .clear
+        triangle.setNeedsDisplay()
+
+        triangle.snp.makeConstraints { make in
+            make.top.equalTo(tableView.visibleCells[1].snp.bottom).offset(16)
+            make.centerX.equalTo(view.snp.centerX)
+            make.width.equalTo(18)
+            make.height.equalTo(16)
+        }
+        
+        bubbleView.addSubview(bubbleLabel)
+
+        bubbleView.snp.makeConstraints { make in
+            make.top.equalTo(triangle.snp.bottom).offset(-4)
+            make.centerX.equalTo(view.snp.centerX)
+            make.trailing.equalTo(view.snp.trailing).offset(-54)
+            make.leading.equalTo(view.snp.leading).offset(54)
+            make.height.equalTo(36)
+        }
+        
+        bubbleLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(bubbleView.snp.centerY)
+            make.centerX.equalTo(bubbleView.snp.centerX)
+        }
     }
 }
 
