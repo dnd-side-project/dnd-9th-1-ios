150abd371a68ecdc2e8f28e392eab4221b93396d
EunsuSeo01 <eunsu0613@naver.com>
2023-08-17 00:51:59 +0900

    [Feat] #38 목표 시작일과 종료일이 조건에 맞지 않는 경우 에러 메세지 띄우는 기능 구현
    시작일은 종료일보다 앞서거나 같을 수 있다

--- Milestone/Milestone/Screens/FillBox/View/EnterGoalDateView.swift
+++ Milestone/Milestone/Screens/FillBox/View/EnterGoalDateView.swift
@@ -55,6 +55,13 @@ class EnterGoalDateView: UIView {
             $0.addTarget(self, action: #selector(presentEndAlert), for: .touchUpInside)
             $0.setTitle("\(dateFormatter.string(from: endDate))", for: .normal)
         }
+    var dateErrorLabel = UILabel()
+        .then {
+            $0.text = "시작일과 종료일을 다시 확인해주세요"
+            $0.textColor = .pointRed
+            $0.textAlignment = .right
+            $0.font = .pretendard(.regular, ofSize: 10)
+        }
     
     // - 1. 시작일을 위한 alert
     lazy var startDatePicker = UIDatePicker()
@@ -71,8 +78,10 @@ class EnterGoalDateView: UIView {
         }
     /// 종료일 세팅하기 전에는 시작일 세팅할 때에 종료일이 + 7일인 것으로 자동 설정
     lazy var completeStartDateAction = UIAlertAction(title: "완료", style: .default) { [self] _ in
-        afterEndDatePicker.date = startDatePicker.date
-        endDatePicker.date = startDatePicker.date.addingTimeInterval(sevenDaysInterval)
+        startDate = startDatePicker.date
+        endDate = startDatePicker.date.addingTimeInterval(sevenDaysInterval)
+        afterEndDatePicker.date = startDate
+        endDatePicker.date = endDate
         self.startDateButton.setTitle("\(self.dateFormatter.string(from: startDatePicker.date))", for: .normal)
         self.endDateButton.setTitle("\(dateFormatter.string(from: startDatePicker.date.addingTimeInterval(sevenDaysInterval)))", for: .normal)
     }
@@ -90,6 +99,7 @@ class EnterGoalDateView: UIView {
             $0.addAction(completeEndDateAction)
         }
     lazy var completeEndDateAction = UIAlertAction(title: "완료", style: .default) { [self] _ in
+        endDate = endDatePicker.date
         self.endDateButton.setTitle("\(self.dateFormatter.string(from: endDatePicker.date))", for: .normal)
     }
     // - 3. 종료일 설정 이후의 시작일을 위한 alert
@@ -107,6 +117,7 @@ class EnterGoalDateView: UIView {
         }
     /// 종료일 세팅한 후에는 종료일 건들지 말고 시작일만 설정
     lazy var completeStartDateAfterEndDateSettingAction = UIAlertAction(title: "완료", style: .default) { [self] _ in
+        startDate = afterEndDatePicker.date
         self.startDateButton.setTitle("\(self.dateFormatter.string(from: afterEndDatePicker.date))", for: .normal)
     }
         
@@ -116,13 +127,22 @@ class EnterGoalDateView: UIView {
         .then {
             $0.dateFormat = "yyyy / MM / dd"
         }
-    var startDate = Date()
+    var startDate = Date() {
+        didSet {
+            checkDateCondition()
+        }
+    }
     /// 1일 = 24시간 * 60분 * 60초
     let sevenDaysInterval: TimeInterval = 7 * 24 * 60 * 60
-    lazy var endDate = startDate.addingTimeInterval(sevenDaysInterval)
+    lazy var endDate = startDate.addingTimeInterval(sevenDaysInterval) {
+        didSet {
+            checkDateCondition()
+        }
+    }
     var alertMessage = ""
     /// 종료일 세팅한 후인지 확인하기 위한 변수
     var isAfterEndDateSetting = false
+    var isErrorDate = false
     weak var delegate: PresentAlertDelegate?
     
     let vc = UIViewController()
@@ -184,6 +204,39 @@ class EnterGoalDateView: UIView {
         vc.view = startDatePicker
         vc2.view = endDatePicker
         vc3.view = afterEndDatePicker
+    }
+    
+    /// 시작일이 종료일보다 앞서거나 같은지 확인
+    private func checkDateCondition() {
+        if startDate <= endDate {
+            if isErrorDate {
+                setOriginDateStyle()
+            }
+        } else { // ERROR!
+            setErrorDateStyle()
+        }
+    }
+    
+    /// 시작일이 종료일보다 나중일 경우 에러 메세지 띄우고 전체 높이 변경
+    private func setErrorDateStyle() {
+        isErrorDate = true
+        addSubview(dateErrorLabel)
+        dateErrorLabel.snp.makeConstraints { make in
+            make.top.equalTo(endDateButton.snp.bottom).offset(4)
+            make.right.equalTo(endDateButton)
+        }
+        self.snp.updateConstraints { make in
+            make.height.equalTo(121)
+        }
+    }
+    
+    /// 에러였다가 다시 조건을 만족시킨 경우 에러 메세지 지우고 전체 높이 변경
+    private func setOriginDateStyle() {
+        isErrorDate = false
+        dateErrorLabel.removeFromSuperview()
+        self.snp.updateConstraints { make in
+            make.height.equalTo(104)
+        }
     }
     
     // MARK: - @objc Functions
