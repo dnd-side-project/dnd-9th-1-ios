5901d828135d31e33d2dd623affa26f61ad808d9
Parkjju <rudwns3927@gmail.com>
2023-08-17 02:55:51 +0900

    [Fix] #48 커스텀 디스포저블 옵셔널로 타입 수정

--- Milestone/Milestone/Screens/CompletionBox/View/CompletionBoxViewController.swift
+++ Milestone/Milestone/Screens/CompletionBox/View/CompletionBoxViewController.swift
@@ -61,8 +61,8 @@ class CompletionBoxViewController: BaseViewController, ViewModelBindableType {
         }
     
     var tapDisposable: [Disposable] = []
-    var cellHideDisposable: Disposable!
-    var nsAttributedStringDisposable: Disposable!
+    var cellHideDisposable: Disposable?
+    var nsAttributedStringDisposable: Disposable?
     
     // MARK: Properties
     
@@ -121,8 +121,8 @@ class CompletionBoxViewController: BaseViewController, ViewModelBindableType {
         }
         tapDisposable = []
         
-        cellHideDisposable.dispose()
-        nsAttributedStringDisposable.dispose()
+        cellHideDisposable?.dispose()
+        nsAttributedStringDisposable?.dispose()
     }
     
     // MARK: functions
