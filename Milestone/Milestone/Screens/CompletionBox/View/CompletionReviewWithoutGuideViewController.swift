97ae2982c8cad1bdbd7f96749b68422516c94a5c
Parkjju <rudwns3927@gmail.com>
2023-08-16 19:03:21 +0900

    [Feat] #43 자유롭게 회고하기 뷰 임시생성

---
+++ Milestone/Milestone/Screens/CompletionBox/View/CompletionReviewWithoutGuideViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CompletionReviewWithoutGuideViewController.swift
+//  Milestone
+//
+//  Created by 박경준 on 2023/08/16.
+//
+
+import UIKit
+
+class CompletionReviewWithoutGuideViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+
+}
