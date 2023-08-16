81362bb842c1d4aa2452769a8f651308ae07d118
Parkjju <rudwns3927@gmail.com>
2023-08-16 18:56:51 +0900

    [Style] #43 Textfield 익스텐션함수 LeftPadding 코드 컨벤션 맞춰서 수정

--- Milestone/Milestone/Global/Extension/UITextField+.swift
+++ Milestone/Milestone/Global/Extension/UITextField+.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 extension UITextField {
-    func setLeftPaddingPoints(_ amount:CGFloat){
+    func setLeftPaddingPoints(_ amount: CGFloat) {
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
         self.leftView = paddingView
         self.leftViewMode = .always
     }
-    func setRightPaddingPoints(_ amount:CGFloat) {
+    func setRightPaddingPoints(_ amount: CGFloat) {
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
         self.rightView = paddingView
         self.rightViewMode = .always
