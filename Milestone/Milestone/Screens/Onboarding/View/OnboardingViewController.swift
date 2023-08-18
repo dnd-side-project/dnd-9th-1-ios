352992adf0b5994cc3bbba2123e22421dc7b53f3
Parkjju <rudwns3927@gmail.com>
2023-08-19 00:18:56 +0900

    [Style] #58 UI속성 지정 코드 then으로 이동

--- Milestone/Milestone/Screens/Onboarding/View/OnboardingViewController.swift
+++ Milestone/Milestone/Screens/Onboarding/View/OnboardingViewController.swift
@@ -43,6 +43,7 @@ class OnboardingViewController: BaseViewController {
     
     lazy var completeButton = RoundedDarkButton()
         .then {
+            $0.titleString = "목표 만들기 완료"
             $0.addTarget(self, action: #selector(completeButtonTapped), for: .touchUpInside)
         }
     
@@ -83,8 +84,6 @@ class OnboardingViewController: BaseViewController {
         view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         view.layer.cornerRadius = 20
         view.makeShadow(color: .init(hex: "#464646", alpha: 0.2), alpha: 1, x: 0, y: -10, blur: 20, spread: 0)
-        
-        completeButton.titleString = "목표 만들기 완료"
     }
     
     // MARK: Objc functions
