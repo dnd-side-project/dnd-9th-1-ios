c975127dbb8206e34c00233a9a733135eb39c44e
Parkjju <rudwns3927@gmail.com>
2023-08-19 22:38:48 +0900

    [Feat] #70 목표달성 현황 뷰 제작

---
+++ Milestone/Milestone/Screens/Shared/GoalAchievementRateView.swift
@@ -0,0 +1,67 @@
+//
+//  GoalAchievementRateView.swift
+//  Milestone
+//
+//  Created by 박경준 on 2023/08/19.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+class GoalAchievementRateView: UIView {
+    
+    // MARK: Subviews
+    private let countLabel = UILabel()
+        .then {
+            $0.textColor = .primary
+            $0.font = .pretendard(.semibold, ofSize: 14)
+        }
+    
+    // MARK: Properties
+    var completedCount: CGFloat = 0
+    var totalCompletedCount: CGFloat = 0
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        render()
+        configUI()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Functions
+    
+    override func draw(_ rect: CGRect) {
+        
+        let radian = 270 - 360 * (completedCount / totalCompletedCount)
+        
+        self.countLabel.text = "\(Int(completedCount))/\(Int(totalCompletedCount))"
+        
+        let path = UIBezierPath(arcCenter: CGPoint(x: rect.midX, y: rect.midY), radius: 22, startAngle: (270 * .pi) / 180, endAngle: (radian * .pi) / 180, clockwise: false)
+
+        path.lineWidth = 6
+        UIColor.primary.set()
+        path.stroke()
+    }
+    
+    func render() {
+        self.addSubview(countLabel)
+        
+        countLabel.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
+    }
+    
+    func configUI() {
+        self.clipsToBounds = false
+        self.backgroundColor = .systemBackground
+    }
+}
