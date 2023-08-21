09de9d0c2f9402204e71e31e4586d0636f266754
Parkjju <rudwns3927@gmail.com>
2023-08-21 20:50:22 +0900

    [Feat] #101 키값 로드 / SDK 초기화 및 로그인페이지 오픈 로직 추가

--- Milestone/Milestone/Global/Support/AppDelegate.swift
+++ Milestone/Milestone/Global/Support/AppDelegate.swift
@@ -7,17 +7,34 @@
 
 import UIKit
 
+import KakaoSDKAuth
+import RxKakaoSDKAuth
+import RxKakaoSDKCommon
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var coordinator: AppCoordinator?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        if let kakaoKey = ProcessInfo.processInfo.environment["KAKAO_NATIVE_KEY"] {
+            RxKakaoSDK.initSDK(appKey: kakaoKey)
+        }
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = .systemBackground
         coordinator = AppCoordinator(window: window!)
         coordinator?.start()
         
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if AuthApi.isKakaoTalkLoginUrl(url) {
+            return AuthController.rx.handleOpenUrl(url: url)
+        }
+        
+        return false
     }
 }
