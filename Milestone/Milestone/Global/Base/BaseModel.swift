776b02021830e66177a0b090d4699efbd4e2b5ae
Parkjju <rudwns3927@gmail.com>
2023-08-21 14:49:25 +0900

    [Fix] #60 더미데이터 모델 삭제

---
+++ Milestone/Milestone/Global/Base/BaseModel.swift
@@ -0,0 +1,8 @@
+//
+//  BaseModel.swift
+//  Milestone
+//
+//  Created by 박경준 on 2023/08/21.
+//
+
+import Foundation
---
+++ Milestone/Milestone/Model/Goal.swift
@@ -0,0 +1,8 @@
+//
+//  Goal.swift
+//  Milestone
+//
+//  Created by 박경준 on 2023/08/21.
+//
+
+import Foundation
