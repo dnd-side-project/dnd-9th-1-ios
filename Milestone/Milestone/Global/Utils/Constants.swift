4c8789266dabbfeec6d7320470969b5ed417c64d
Parkjju <rudwns3927@gmail.com>
2023-08-21 14:51:28 +0900

    [Fix] #60 baseURL 수정 및 쿼리 파라미터 문자열 리스트 추가

--- Milestone/Milestone/Global/Utils/Constants.swift
+++ Milestone/Milestone/Global/Utils/Constants.swift
@@ -10,14 +10,18 @@ import Foundation
 struct K {
     
     /// API base URL
-    static let baseUrl = "https://jsonplaceholder.typicode.com"
+    static let baseUrl = "https://dnd9th.site"
     
     /// 리퀘스트 바디 파라미터의 키값을 문자열로 사용할때 직접 추가
     struct Parameters {
-        static let id = "id"
+        static let goalId = "goalId"
         static let title = "title"
-        static let body = "body"
-        static let userId = "userId"
+        static let startDate = "startDate"
+        static let endDate = "endDate"
+        static let reminderEnabled = "reminderEnabled"
+        static let alarmEnabled = "alarmEnabled"
+        static let alarmTime = "alarmTime"
+        static let alarmDays = "alarmDays"
     }
     
     /// 헤더 필드
