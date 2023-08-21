372fa339c65779d652bf3b6577db9d986964b57c
Parkjju <rudwns3927@gmail.com>
2023-08-21 14:49:57 +0900

    [Feat] #60 API 리스트 최종 정리

--- Milestone/Milestone/Core/API/APIRouter.swift
+++ Milestone/Milestone/Core/API/APIRouter.swift
@@ -12,33 +12,171 @@ import Alamofire
 enum APIRouter: URLRequestConvertible {
     
     /// 엔드포인트 리스트
-    case retrieveAllGoals
+    /// 상위목표 관련 API 리스트
+    case requestAllGoals(goalStatus: GoalStatusParameter)
+    case deleteGoal(id: Int)
+    case requestEnabledRetrospectCount
+    case requestGoalCountByStatus
+    case editGoal(id: Int, goal: Goal)
+    case recoverGoal(id: Int, startDate: String, endDate: String, reminderEnabled: Bool)
+    case postGoal(goal: Goal)
+    
+    /// 유저 관련 API 리스트
+    case reissue
+    case login(provider: String, userId: String)
+    
+    /// 하위목표 관련 API 리스트
+    case deleteDetailGoal(lowerLevelGoalId: Int)
+    case requestAllDetailGoal(higherLevelGoalId: Int)
+    case requestDetailGoalInformation(lowerLevelGoalId: Int)
+    case editDetailGoal(lowerLevelGoalId: Int, detailGoal: DetailGoal)
+    case incompleteDetailGoal(lowerLevelGoalId: Int) // 하위목표 완료 취소
+    case completeDetailGoal(lowerLevelGoalId: Int) // 하위목표 완료
+    case postDetailGoal(higherLevelGoalId: Int, detailGoal: DetailGoal)
     
     // MARK: - HttpMethod
     /// switch - self 구문으로 각 엔드포인트별 메서드 지정
     private var method: HTTPMethod {
         switch self {
-        case .retrieveAllGoals:
+        case .requestAllGoals:
             return .get
+        case .deleteGoal:
+            return .delete
+        case .requestEnabledRetrospectCount:
+            return .get
+        case .requestGoalCountByStatus:
+            return .get
+        case .editGoal:
+            return .patch
+        case .recoverGoal:
+            return .patch
+        case .postGoal:
+            return .post
+        case .reissue:
+            return .post
+        case .login:
+            return .post
+        case .deleteDetailGoal:
+            return .delete
+        case .requestAllDetailGoal:
+            return .get
+        case .requestDetailGoalInformation:
+            return .get
+        case .editDetailGoal:
+            return .patch
+        case .incompleteDetailGoal:
+            return .patch
+        case .completeDetailGoal:
+            return .patch
+        case .postDetailGoal:
+            return .post
         }
     }
     
-    //MARK: - Path
+    // MARK: - Path
+    
     /// switch - self 구문으로 각 엔드포인트별 URL Path 지정
     private var path: String {
         switch self {
-        case .retrieveAllGoals:
+        case .requestAllGoals(let goalStatus):
+            return "/goals?goalStatus=\(goalStatus.rawValue)"
+        case .deleteGoal(let id):
+            return "/goals/\(id)"
+        case .requestEnabledRetrospectCount:
+            return "/goals/retrospect-enabled/count"
+        case .requestGoalCountByStatus:
+            return "/goals/count"
+        case .editGoal(let id, _):
+            return "/goals/\(id)"
+        case .recoverGoal(let id, _, _, _):
+            return "/goals/\(id)/recover"
+        case .postGoal:
             return "/goals"
+        case .reissue:
+            return "/reissue"
+        case .login(let provider, let userId):
+            return "/auth/\(provider)?userId=\(userId)"
+        case .deleteDetailGoal(let id):
+            return "/detail-goals/\(id)"
+        case .requestAllDetailGoal(let id):
+            return "/goals/\(id)/detail-goals"
+        case .requestDetailGoalInformation(let id):
+            return "/detail-goals/\(id)"
+        case .editDetailGoal(let id, _):
+            return "/detail-goals/\(id)"
+        case .incompleteDetailGoal(let id):
+            return "/detail-goals/\(id)/incomplete"
+        case .completeDetailGoal(let id):
+            return "/detail-goals/\(id)/complete"
+        case .postDetailGoal(let id, _):
+            return "/goals/\(id)/detail-goals"
         }
     }
     
-    //MARK: - Parameters
+    // MARK: - Parameters
+    
     /// request body 정의
     /// 빈 body를 보낼때는 nil값 전달
     private var parameters: Parameters? {
         switch self {
-        case .retrieveAllGoals:
+        case .requestAllGoals:
             return nil
+        case .deleteGoal:
+            return nil
+        case .requestEnabledRetrospectCount:
+            return nil
+        case .requestGoalCountByStatus:
+            return nil
+        case .editGoal(_, let goal):
+            return [
+                K.Parameters.goalId: goal.identity,
+                K.Parameters.title: goal.title,
+                K.Parameters.startDate: goal.startDate,
+                K.Parameters.endDate: goal.endDate,
+                K.Parameters.reminderEnabled: goal.reminderEnabled
+            ]
+        case .recoverGoal(let id, let startDate, let endDate, let reminderEnabled):
+            return [
+                K.Parameters.goalId: id,
+                K.Parameters.startDate: startDate,
+                K.Parameters.endDate: endDate,
+                K.Parameters.reminderEnabled: reminderEnabled
+            ]
+        case .postGoal(let goal):
+            return [
+                K.Parameters.title: goal.title,
+                K.Parameters.startDate: goal.startDate,
+                K.Parameters.endDate: goal.endDate,
+                K.Parameters.reminderEnabled: goal.reminderEnabled
+            ]
+        case .reissue:
+            return nil
+        case .login:
+            return nil
+        case .deleteDetailGoal:
+            return nil
+        case .requestAllDetailGoal:
+            return nil
+        case .requestDetailGoalInformation:
+            return nil
+        case .editDetailGoal(_, let detailGoal):
+            return [
+                K.Parameters.title: detailGoal.title,
+                K.Parameters.alarmEnabled: detailGoal.alarmEnabled,
+                K.Parameters.alarmTime: detailGoal.alarmTime,
+                K.Parameters.alarmDays: detailGoal.alarmDays
+            ]
+        case .incompleteDetailGoal:
+            return nil
+        case .completeDetailGoal:
+            return nil
+        case .postDetailGoal(_, let detailGoal):
+            return [
+                K.Parameters.title: detailGoal.title,
+                K.Parameters.alarmEnabled: detailGoal.alarmEnabled,
+                K.Parameters.alarmTime: detailGoal.alarmTime,
+                K.Parameters.alarmDays: detailGoal.alarmDays
+            ]
         }
     }
     
