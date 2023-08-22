01575d2a3cabb64273a0e1c93c07aeafbe5fa655
Parkjju <rudwns3927@gmail.com>
2023-08-22 16:35:02 +0900

    [Feat] #101 카카오톡 로그인 여부 체크 후 메인화면으로 이동

--- Milestone/Milestone/Global/Support/AppCoordinator.swift
+++ Milestone/Milestone/Global/Support/AppCoordinator.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+
+import KakaoSDKAuth
+import RxKakaoSDKAuth
 import RxSwift
 
 class AppCoordinator: Coordinator {
@@ -15,12 +18,18 @@ class AppCoordinator: Coordinator {
         self.window = window
     }
     
+    /// 카카오 로그인 여부 & 애플 로그인 여부 체크 후 메인화면으로 이동
+    /// 로그아웃 이후에는 각각 revoke시킨 뒤 로그인으로 이동시키도록 분기처리 필요
     func start() {
         let navigationController = UINavigationController()
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
         
-        let startCoordinator = LoginCoordinator(navigationController: navigationController)
-        coordinate(to: startCoordinator)
+        if AuthApi.hasToken() {
+            navigationController.pushViewController(MainViewController(), animated: true)
+        } else {
+            let startCoordinator = LoginCoordinator(navigationController: navigationController)
+            coordinate(to: startCoordinator)
+        }
     }
 }
