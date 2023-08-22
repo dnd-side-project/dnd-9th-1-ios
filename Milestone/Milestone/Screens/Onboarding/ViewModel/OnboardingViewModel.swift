5624b09c113a22ae66e126197e9523094f0c46b7
Parkjju <rudwns3927@gmail.com>
2023-08-22 16:02:04 +0900

    [Feat] #101 온보딩 뷰모델 코디네이터 추가 / 로그인메서드 구현

--- Milestone/Milestone/Screens/Onboarding/ViewModel/OnboardingViewModel.swift
+++ Milestone/Milestone/Screens/Onboarding/ViewModel/OnboardingViewModel.swift
@@ -7,6 +7,62 @@
 
 import Foundation
 
-class OnboardingViewModel {
+import KakaoSDKUser
+import RxSwift
+import RxKakaoSDKUser
+
+class OnboardingViewModel: BindableViewModel {
+    var bag = DisposeBag()
+    var apiSession: APIService = APISession()
+    let idSubject = PublishSubject<String>()
+    let tokenSubject = PublishSubject<Token>()
+    var loginCoordinator: LoginFlow?
     
+    func login() {
+        let fcmObservable = KeychainManager.shared.rx
+            .retrieveItem(ofClass: .password, key: KeychainKeyList.fcmToken.rawValue)
+            .compactMap { $0 as String }
+        
+        UserApi.shared.rx.me()
+            .subscribe(onSuccess: { [unowned self] in
+                guard let id = $0.id else { return }
+                self.idSubject.onNext("\(id)")
+            })
+            .disposed(by: bag)
+        
+        Observable.combineLatest(idSubject.asObservable(), fcmObservable) { [unowned self] userId, fcmToken in
+            self.requestToken(provider: "kakao", userId: userId, fcmToken: fcmToken)
+        }
+        .flatMap { $0 }
+        .subscribe(onNext: { [unowned self] result in
+            switch result {
+            case .success(let response):
+                self.tokenSubject.onNext(response.data)
+            case .failure(let error):
+                print(error)
+            }
+        })
+        .disposed(by: bag)
+        
+        let mergedObservable = Observable.of(
+            tokenSubject
+                .map {
+                    KeychainManager.shared.rx
+                        .saveItem($0.accessToken, itemClass: .password, key: KeychainKeyList.accessToken.rawValue)
+                },
+            tokenSubject
+                .map {
+                    KeychainManager.shared.rx
+                        .saveItem($0.refreshToken, itemClass: .password, key: KeychainKeyList.refreshToken.rawValue)
+                })
+        
+        Observable.merge(mergedObservable)
+            .subscribe(onCompleted: { [weak self] in
+                guard let self = self else { return }
+                loginCoordinator?.coordinateToOnboarding()
+            })
+            .disposed(by: bag)
+    }
 }
+
+extension OnboardingViewModel: ServicesUser { }
