cfee42c18c97c786c7a3658691124ec304577783
Parkjju <rudwns3927@gmail.com>
2023-08-22 15:41:33 +0900

    [Feat] #101 API 인터셉터 클래스 정의
    1. 추후 adapt & retry 메서드 추가 예정

---
+++ Milestone/Milestone/Core/API/APIInterceptor.swift
@@ -0,0 +1,20 @@
+//
+//  APIInterceptor.swift
+//  Milestone
+//
+//  Created by 박경준 on 2023/08/22.
+//
+
+import Foundation
+
+import Alamofire
+
+class APIInterceptor: RequestInterceptor {
+//    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+//        <#code#>
+//    }
+//    
+//    func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
+//        <#code#>
+//    }
+}
