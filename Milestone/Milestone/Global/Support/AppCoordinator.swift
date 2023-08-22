b33e56063242a290df2ef4cce43214a1d489c54d
Parkjju <rudwns3927@gmail.com>
2023-08-22 17:11:27 +0900

    [Test] #101 테스트환경 유지 코드 추가

--- Milestone/Milestone/Global/Support/AppCoordinator.swift
+++ Milestone/Milestone/Global/Support/AppCoordinator.swift
@@ -25,11 +25,14 @@ class AppCoordinator: Coordinator {
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
         
-        if AuthApi.hasToken() {
-            navigationController.pushViewController(MainViewController(), animated: true)
-        } else {
-            let startCoordinator = LoginCoordinator(navigationController: navigationController)
-            coordinate(to: startCoordinator)
-        }
+        // FIXME: 실물기기 테스트 진행 가능시 아래 주석처리 코드로 수정할것!
+        navigationController.pushViewController(MainViewController(), animated: true)
+        
+//        if AuthApi.hasToken() {
+//            navigationController.pushViewController(MainViewController(), animated: true)
+//        } else {
+//            let startCoordinator = LoginCoordinator(navigationController: navigationController)
+//            coordinate(to: startCoordinator)
+//        }
     }
 }
