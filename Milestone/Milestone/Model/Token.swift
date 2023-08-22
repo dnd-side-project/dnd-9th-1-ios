492ac48aaa65c2519f9b3cbf6425d218df7cf021
Parkjju <rudwns3927@gmail.com>
2023-08-22 15:47:32 +0900

    [Feat] #101 토큰모델 정의

---
+++ Milestone/Milestone/Model/Token.swift
@@ -0,0 +1,13 @@
+//
+//  Token.swift
+//  Milestone
+//
+//  Created by 박경준 on 2023/08/22.
+//
+
+import Foundation
+
+struct Token: Codable {
+    let accessToken: String
+    let refreshToken: String
+}
