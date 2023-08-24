1eaaa5d695e0dc77b80baee5bded4dc0ce07b9c5
Parkjju <rudwns3927@gmail.com>
2023-08-24 22:48:18 +0900

    [Feat] #125 테이블뷰 레지스터 및 데이터소스 설정

--- Milestone/Milestone/Screens/Setting/SettingViewController.swift
+++ Milestone/Milestone/Screens/Setting/SettingViewController.swift
@@ -13,8 +13,11 @@ class SettingViewController: BaseViewController {
     
     lazy var settingTableView = UITableView()
         .then {
-            $0.delegate = self
+            $0.separatorStyle = .none
+            $0.register(cell: SettingTableViewCellFirstSection.self, forCellReuseIdentifier: SettingTableViewCellFirstSection.identifier)
+            $0.register(cell: SettingTableViewCellSecondSection.self, forCellReuseIdentifier: SettingTableViewCellSecondSection.identifier)
             $0.dataSource = self
+            $0.delegate = self
         }
     
     lazy var leftBarButton = UIBarButtonItem()
@@ -29,6 +32,7 @@ class SettingViewController: BaseViewController {
     // MARK: - Properties
     
     let cellItems = [["푸시 알림", "리마인드 알림"], ["개인정보 처리 방침", "로그아웃", "회원 탈퇴"]]
+    let sectionItems = ["알림", "계정 관리"]
     
     // MARK: - Functions
     
@@ -49,18 +53,25 @@ class SettingViewController: BaseViewController {
 }
 
 extension SettingViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return cellItems.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        cellItems[section].count
+        return cellItems[section].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch indexPath.section {
         case 0:
+            print("case: \(indexPath.section)")
             guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCellFirstSection.identifier) as? SettingTableViewCellFirstSection else { return UITableViewCell() }
-            cell.label.text = cellItems[indexPath.section][indexPath.item]
+            print( cellItems[indexPath.section][indexPath.row])
+            cell.label.text = cellItems[indexPath.section][indexPath.row]
             return cell
         case 1:
             guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCellSecondSection.identifier) as? SettingTableViewCellSecondSection else { return UITableViewCell() }
+            cell.label.text = cellItems[indexPath.section][indexPath.row]
             return cell
         default:
             return UITableViewCell()
@@ -69,5 +80,32 @@ extension SettingViewController: UITableViewDataSource {
 }
 
 extension SettingViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 56 + 8
+    }
     
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let headerContainerView = UIView()
+        
+        let sectionLabel = UILabel()
+            .then {
+                $0.textColor = .gray03
+                $0.font = .pretendard(.semibold, ofSize: 12)
+                $0.text = sectionItems[section]
+            }
+        
+        headerContainerView.addSubview(sectionLabel)
+        
+        sectionLabel.snp.makeConstraints { make in
+            make.leading.equalTo(headerContainerView.snp.leading).offset(24)
+            make.centerY.equalTo(headerContainerView.snp.centerY)
+        }
+        
+        return headerContainerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 48
+    }
 }
