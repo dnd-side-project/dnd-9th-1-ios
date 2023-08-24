bd19ebb1cba92578cd4af72eaf8936182e05b2e0
EunsuSeo01 <eunsu0613@naver.com>
2023-08-24 19:18:42 +0900

    [Feat] #124 세부 목표 생성 관련 서비스 함수 작성

--- Milestone/Milestone/Core/Services/ServicesDetailGoal.swift
+++ Milestone/Milestone/Core/Services/ServicesDetailGoal.swift
@@ -14,11 +14,15 @@ import RxSwift
 protocol ServicesDetailGoal: Service {
     // 세부 목표 리스트 조회 API 요청
     func requestDetailGoalList(id: Int) -> Observable<Result<BaseModel<[DetailGoal]>, APIError>>
-    
+    // 세부 목표 생성 API 요청
+    func requestPostDetailGoal(id: Int, reqBody: DetailGoalInfo) -> Observable<Result<EmptyDataModel, APIError>>
 }
 
 extension ServicesDetailGoal {
     func requestDetailGoalList(id: Int) -> Observable<Result<BaseModel<[DetailGoal]>, APIError>> {
         return apiSession.request(.requestAllDetailGoal(higherLevelGoalId: id))
+    }
+    func requestPostDetailGoal(id: Int, reqBody: DetailGoalInfo) -> Observable<Result<EmptyDataModel, APIError>> {
+        return apiSession.request(.postDetailGoal(higherLevelGoalId: id, detailGoal: reqBody))
     }
 }
