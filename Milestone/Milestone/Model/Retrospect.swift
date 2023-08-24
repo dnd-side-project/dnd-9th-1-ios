083d330ac5ccba34d065c4b02f3088f0034aa043
Parkjju <rudwns3927@gmail.com>
2023-08-24 21:03:50 +0900

    [Feat] #113 회고모델 디코딩 함수 커스텀

--- Milestone/Milestone/Model/Retrospect.swift
+++ Milestone/Milestone/Model/Retrospect.swift
@@ -15,6 +15,19 @@ struct Retrospect: Codable {
     enum CodingKeys: String, CodingKey {
         case hasGuide = "has_guide"
         case contents = "contents"
-        case successLevel = "success_level"
+        case successLevel = "successLevel"
+    }
+    
+    init(hasGuide: Bool, contents: [String: String], successLevel: String) {
+        self.hasGuide = hasGuide
+        self.contents = contents
+        self.successLevel = successLevel
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.hasGuide = (try? container.decode(Bool.self, forKey: .hasGuide)) ?? false
+        self.contents = try container.decode([String : String].self, forKey: .contents)
+        self.successLevel = try container.decode(String.self, forKey: .successLevel)
     }
 }
