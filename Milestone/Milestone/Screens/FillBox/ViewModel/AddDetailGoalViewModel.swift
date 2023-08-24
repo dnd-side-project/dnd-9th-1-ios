d1af271ac158a885397b4acc6d12d7b09392a143
EunsuSeo01 <eunsu0613@naver.com>
2023-08-24 19:40:44 +0900

    [Feat] #124 세부 목표 추가 ViewModel 작성

---
+++ Milestone/Milestone/Screens/FillBox/ViewModel/AddDetailGoalViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  AddDetailGoalViewModel.swift
+//  Milestone
+//
+//  Created by 서은수 on 2023/08/24.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+// MARK: - 세부 목표 추가 화면 VM
+
+class AddDetailGoalViewModel: BindableViewModel, ServicesDetailGoal {
+    
+    // MARK: - BindableViewModel Properties
+    
+    var apiSession: APIService = APISession()
+    var bag = DisposeBag()
+        
+    // MARK: - Properties
+    
+    var parentGoalId: Int = 0
+    
+    deinit {
+        bag = DisposeBag()
+    }
+}
+
+extension AddDetailGoalViewModel {
+    func createDetailGoal(reqBody: DetailGoalInfo) {
+        var createDetailGoalResponse: Observable<Result<EmptyDataModel, APIError>> {
+            requestPostDetailGoal(id: parentGoalId, reqBody: reqBody)
+        }
+        
+        createDetailGoalResponse
+            .subscribe(onNext: { result in
+                switch result {
+                case .success(let response):
+                    Logger.debugDescription(response)
+                case .failure(let error):
+                    Logger.debugDescription(error)
+                }
+            })
+            .disposed(by: bag)
+    }
+}
