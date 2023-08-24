2bd4e34adb41f3e1c1517a3ab33fb9ad9b42aeca
Parkjju <rudwns3927@gmail.com>
2023-08-25 05:40:35 +0900

    [Fix] #113 회고모델 decoding strategy 수정

--- Milestone/Milestone/Model/Retrospect.swift
+++ Milestone/Milestone/Model/Retrospect.swift
@@ -11,25 +11,6 @@ struct Retrospect: Codable {
     let hasGuide: Bool
     let contents: [String: String]
     let successLevel: String
-    
-    enum CodingKeys: String, CodingKey {
-        case hasGuide = "has_guide"
-        case contents = "contents"
-        case successLevel = "successLevel"
-    }
-    
-    init(hasGuide: Bool, contents: [String: String], successLevel: String) {
-        self.hasGuide = hasGuide
-        self.contents = contents
-        self.successLevel = successLevel
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.hasGuide = (try? container.decode(Bool.self, forKey: .hasGuide)) ?? false
-        self.contents = try container.decode([String : String].self, forKey: .contents)
-        self.successLevel = try container.decode(String.self, forKey: .successLevel)
-    }
 }
 
 struct RetrospectCount: Codable {
