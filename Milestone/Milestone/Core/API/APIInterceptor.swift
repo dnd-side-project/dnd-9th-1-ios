7475b82dc7d863694312216f433cf2b3ef9364fd
Parkjju <rudwns3927@gmail.com>
2023-08-25 23:05:21 +0900

    [Feat] #136 인터셉터 회원가입 API 분기처리 및 액세스토큰 헤더 세팅

--- Milestone/Milestone/Core/API/APIInterceptor.swift
+++ Milestone/Milestone/Core/API/APIInterceptor.swift
@@ -8,13 +8,35 @@
 import Foundation
 
 import Alamofire
+import RxSwift
 
 class APIInterceptor: RequestInterceptor {
-//    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
-//        <#code#>
-//    }
-//    
+    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+        
+        if checkIsRegisterAPI(urlRequest: urlRequest) {
+            completion(.success(urlRequest))
+            return
+        }
+        
+        guard let accessToken: String = try? KeychainManager.shared.retrieveItem(ofClass: .password, key: KeychainKeyList.accessToken.rawValue) else {
+            print("NIL")
+            return
+        }
+        var urlRequest = urlRequest
+        urlRequest.headers.add(.authorization(bearerToken: accessToken))
+        
+        completion(.success(urlRequest))
+    }
+
 //    func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
 //        <#code#>
 //    }
+    
+    func checkIsRegisterAPI(urlRequest: URLRequest) -> Bool {
+        if urlRequest.url!.relativePath == "/auth/kakao" || urlRequest.url!.relativePath == "/auth/apple" {
+            return true
+        } else {
+            return false
+        }
+    }
 }
