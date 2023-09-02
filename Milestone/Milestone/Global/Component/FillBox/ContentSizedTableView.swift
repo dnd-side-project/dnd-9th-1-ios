c1ddf81935cc023488cffa9045df3df1586f6d03
EunsuSeo01 <eunsu0613@naver.com>
2023-09-03 00:24:43 +0900

    [Feat] #154 데이터 바인딩 및 테이블뷰 높이 동적으로 만들기

---
+++ Milestone/Milestone/Global/Component/FillBox/ContentSizedTableView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentSizedTableView.swift
+//  Milestone
+//
+//  Created by 서은수 on 2023/09/03.
+//
+
+import UIKit
+
+// MARK: - content size에 맞게 높이를 자동 조절하는 테이블뷰
+
+final class ContentSizedTableView: UITableView {
+    override var contentSize:CGSize {
+        didSet {
+            invalidateIntrinsicContentSize()
+        }
+    }
+
+    override var intrinsicContentSize: CGSize {
+        layoutIfNeeded()
+        return CGSize(width: UIView.noIntrinsicMetric,
+                     height: contentSize.height + adjustedContentInset.top)
+    }
+}
