d8b034fba10a5a76221736f97444ba2c055a2927
Parkjju <rudwns3927@gmail.com>
2023-09-05 17:40:47 +0900

    [Feat] #136 로그아웃 / 회원탈퇴 서비스 함수 명세 추가 및 구현

--- Milestone/Milestone/Core/Services/ServicesUser.swift
+++ Milestone/Milestone/Core/Services/ServicesUser.swift
@@ -10,10 +10,10 @@ import RxSwift
 
 protocol ServicesUser: Service {
     func requestToken(provider: String, userId: String, fcmToken: String) -> Observable<Result<BaseModel<Token>, APIError>>
-    
     func requestRefreshToken() -> Observable<Result<BaseModel<Token>, APIError>>
-    
     func authTest() -> Observable<Result<String, APIError>>
+    func logout() -> Observable<Result<BaseModel<EmptyDataModel>, APIError>>
+    func withdraw() -> Observable<Result<BaseModel<EmptyDataModel>, APIError>>
 }
 
 extension ServicesUser {
@@ -27,5 +27,13 @@ extension ServicesUser {
     
     func authTest() -> Observable<Result<String, APIError>> {
         return apiSession.request(.authTest)
+    }
+    
+    func logout() -> Observable<Result<BaseModel<EmptyDataModel>, APIError>> {
+        return apiSession.request(.logout)
+    }
+    
+    func withdraw() -> Observable<Result<BaseModel<EmptyDataModel>, APIError>> {
+        return apiSession.request(.withdraw)
     }
 }
