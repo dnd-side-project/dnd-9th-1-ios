09aa9a4f9a986781a63608ecfec808a06945b2ca
Parkjju <rudwns3927@gmail.com>
2023-09-30 23:13:45 +0900

    [Feat] #178 회고 디테일 뷰모델 정의 - Input Output 기반 텍스트뷰 카운트 & 버튼 활성화 반응형 로직 구현

--- Milestone/Milestone/Screens/CompletionBox/View/RetrospectDetail/RetrospectDetailViewModel.swift
+++ Milestone/Milestone/Screens/CompletionBox/View/RetrospectDetail/RetrospectDetailViewModel.swift
@@ -10,7 +10,10 @@ import Foundation
 import RxCocoa
 import RxSwift
 
-class RetrospectDetailViewModel: ViewModelType {
+class RetrospectDetailViewModel: BindableViewModel, ViewModelType {
+
+    var apiSession: APIService = APISession()
+    var bag = DisposeBag()
     
     let upperGoal: BehaviorRelay<UpperGoal>
     
@@ -18,15 +21,31 @@ class RetrospectDetailViewModel: ViewModelType {
         self.upperGoal = BehaviorRelay<UpperGoal>(value: upperGoal)
     }
     
+    // TODO: - 마음 채움도 선택완료 여부까지 인풋으로 추가 필요
     struct Input {
-        
+        let likedTextViewChanged: Observable<String?>
+        let lackedTextViewChanged: Observable<String?>
+        let learnedTextViewChanged: Observable<String?>
+        let longedForTextViewChanged: Observable<String?>
+        let freeTextViewChanged: Observable<String?>
     }
     
     struct Output {
-        
+        let textViewCount: Driver<(Int?, Int?, Int?, Int?)>
+        let freeTextViewCount: Driver<Int?>
+        let guideViewButtonActivated: Driver<Bool>
+        let freeViewButtonActivated: Driver<Bool>
     }
     
     func transform(input: Input) -> Output {
-        return Output()
+        let textViewCount = Observable.combineLatest(input.likedTextViewChanged.map { $0 ?? "" != "내용을 입력해주세요!" ? $0?.count : 0 }, input.lackedTextViewChanged.map { $0 ?? "" != "내용을 입력해주세요!" ? $0?.count : 0 }, input.learnedTextViewChanged.map { $0 ?? "" != "내용을 입력해주세요!" ? $0?.count : 0 }, input.longedForTextViewChanged.map { $0 ?? "" != "내용을 입력해주세요!" ? $0?.count : 0 }).asDriver(onErrorJustReturn: (0, 0, 0, 0))
+        
+        let guideViewActivated = textViewCount.map { $0.0 ?? 0 > 0 && $0.1 ?? 0 > 0 && $0.2 ?? 0 > 0 && $0.3 ?? 0 > 0 }
+        
+        let freeTextViewCount = input.freeTextViewChanged.map { $0 ?? "" != "자유롭게 회고를 작성해보세요!" ? $0?.count : 0 }.asDriver(onErrorJustReturn: 0)
+
+        let freeViewActivated = freeTextViewCount.map { $0 ?? 0 > 0 }
+        
+        return Output(textViewCount: textViewCount, freeTextViewCount: freeTextViewCount, guideViewButtonActivated: guideViewActivated, freeViewButtonActivated: freeViewActivated)
     }
 }
