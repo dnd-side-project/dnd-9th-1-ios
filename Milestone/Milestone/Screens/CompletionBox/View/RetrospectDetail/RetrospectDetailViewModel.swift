47aafba95cd1a1618d0ed0d6759b975e319a3d6e
Parkjju <rudwns3927@gmail.com>
2023-10-01 15:52:25 +0900

    [Feat] #178 버튼 탭 이후 데이터정제 및 API 요청 함수 작성

--- Milestone/Milestone/Screens/CompletionBox/View/RetrospectDetail/RetrospectDetailViewModel.swift
+++ Milestone/Milestone/Screens/CompletionBox/View/RetrospectDetail/RetrospectDetailViewModel.swift
@@ -21,7 +21,6 @@ class RetrospectDetailViewModel: BindableViewModel, ViewModelType {
         self.upperGoal = BehaviorRelay<UpperGoal>(value: upperGoal)
     }
     
-    // TODO: - 마음 채움도 선택완료 여부까지 인풋으로 추가 필요
     struct Input {
         // 텍스트뷰 입력
         let likedTextViewChanged: Observable<String?>
@@ -31,8 +30,12 @@ class RetrospectDetailViewModel: BindableViewModel, ViewModelType {
         let freeTextViewChanged: Observable<String?>
         
         // 마음채움도 입력
-        let pointSelectedWithGuide: Observable<Void>
-        let pointSelectedWithoutGuide: Observable<Void>
+        let pointSelectedWithGuide: Observable<FillPoint>
+        let pointSelectedWithoutGuide: Observable<FillPoint>
+        
+        // 저장버튼 탭
+        let saveButtonTriggerWithGuide: Observable<Void>
+        let saveButtonTriggerWithoutGuide: Observable<Void>
     }
     
     struct Output {
@@ -40,13 +43,16 @@ class RetrospectDetailViewModel: BindableViewModel, ViewModelType {
         let freeTextViewCount: Driver<Int?>
         let guideViewButtonActivated: Driver<Bool>
         let freeViewButtonActivated: Driver<Bool>
+        
+        let modalPresent: Observable<BaseModel<Int>>
     }
     
     func transform(input: Input) -> Output {
+        
         let textViewCount = Observable.combineLatest(input.likedTextViewChanged.map { $0 ?? "" != "내용을 입력해주세요!" ? $0?.count : 0 }, input.lackedTextViewChanged.map { $0 ?? "" != "내용을 입력해주세요!" ? $0?.count : 0 }, input.learnedTextViewChanged.map { $0 ?? "" != "내용을 입력해주세요!" ? $0?.count : 0 }, input.longedForTextViewChanged.map { $0 ?? "" != "내용을 입력해주세요!" ? $0?.count : 0 }).asDriver(onErrorJustReturn: (0, 0, 0, 0))
         
         let guideViewActivated = input.pointSelectedWithGuide
-            .flatMapLatest {
+            .flatMapLatest { _ in
                 textViewCount.map { $0.0 ?? 0 > 0 && $0.1 ?? 0 > 0 && $0.2 ?? 0 > 0 && $0.3 ?? 0 > 0 }
             }
             .asDriver(onErrorJustReturn: false)
@@ -54,11 +60,44 @@ class RetrospectDetailViewModel: BindableViewModel, ViewModelType {
         let freeTextViewCount = input.freeTextViewChanged.map { $0 ?? "" != "자유롭게 회고를 작성해보세요!" ? $0?.count : 0 }.asDriver(onErrorJustReturn: 0)
 
         let freeViewActivated = input.pointSelectedWithoutGuide
-            .flatMapLatest {
+            .flatMapLatest { _ in
                 freeTextViewCount.map { $0 ?? 0 > 0 }
             }
             .asDriver(onErrorJustReturn: false)
         
-        return Output(textViewCount: textViewCount, freeTextViewCount: freeTextViewCount, guideViewButtonActivated: guideViewActivated, freeViewButtonActivated: freeViewActivated)
+        let saveWithGuide = input.saveButtonTriggerWithGuide
+            .flatMapLatest {
+                Observable.combineLatest(input.pointSelectedWithGuide, input.likedTextViewChanged, input.lackedTextViewChanged, input.learnedTextViewChanged, input.longedForTextViewChanged)
+            }
+            .flatMapLatest { [unowned self] (selectPoint, liked, lacked, learned, longedFor) -> Observable<BaseModel<Int>> in
+                
+                let retrospect = Retrospect(hasGuide: true, contents: ["LIKED": liked ?? "", "LACKED": lacked ?? "", "LEARNED": learned ?? "", "LONGED_FOR": longedFor ?? ""], successLevel: selectPoint.rawValue)
+                return self.postRetrospectSingle(higherLevelGoalId: self.upperGoal.value.goalId, retrospect: retrospect).asObservable()
+            }
+        
+        let saveWithoutGuide = Observable.combineLatest(input.saveButtonTriggerWithoutGuide, Observable.combineLatest(input.pointSelectedWithoutGuide, input.freeTextViewChanged))
+            .flatMapLatest { [unowned self] (_, arg1) -> Observable<BaseModel<Int>> in
+                let (fillPoint, text) = arg1
+                let retrospect = Retrospect(hasGuide: false, contents: ["NONE": text ?? ""], successLevel: fillPoint.rawValue)
+                return self.postRetrospectSingle(higherLevelGoalId: self.upperGoal.value.goalId, retrospect: retrospect).asObservable()
+            }
+        
+        input.saveButtonTriggerWithoutGuide
+            .flatMapLatest {
+                Observable.combineLatest(input.pointSelectedWithoutGuide, input.freeTextViewChanged)
+            }
+            .flatMapLatest { [unowned self] (fillPoint, text) -> Observable<BaseModel<Int>> in
+                let retrospect = Retrospect(hasGuide: false, contents: ["NONE": text ?? ""], successLevel: fillPoint.rawValue)
+                return self.postRetrospectSingle(higherLevelGoalId: self.upperGoal.value.goalId, retrospect: retrospect).asObservable()
+            }
+        
+        let modalPresent = Observable.of(saveWithGuide, saveWithoutGuide)
+            .merge()
+        
+        // MARK: - Retrospect 객체 생성
+        
+        return Output(textViewCount: textViewCount, freeTextViewCount: freeTextViewCount, guideViewButtonActivated: guideViewActivated, freeViewButtonActivated: freeViewActivated, modalPresent: modalPresent)
     }
 }
+
+extension RetrospectDetailViewModel: ServicesGoalList { }
