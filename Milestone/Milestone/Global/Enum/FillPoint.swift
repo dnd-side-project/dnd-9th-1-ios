eac2dcb52a747b42626f5d21c3caffba5fb026b7
Parkjju <rudwns3927@gmail.com>
2023-10-01 15:51:01 +0900

    [Feat] #178 마음 채움도 열거형 정의

---
+++ Milestone/Milestone/Global/Enum/FillPoint.swift
@@ -0,0 +1,17 @@
+//
+//  FillPoint.swift
+//  Milestone
+//
+//  Created by 박경준 on 2023/10/01.
+//
+
+import Foundation
+
+// MARK: - 마음 채움도
+enum FillPoint: String {
+    case level1 = "LEVEL1"
+    case level2 = "LEVEL2"
+    case level3 = "LEVEL3"
+    case level4 = "LEVEL4"
+    case level5 = "LEVEL5"
+}
